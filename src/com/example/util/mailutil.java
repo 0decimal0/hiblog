35c7250c3f5f501622aec94bb4bd078507cbb1bc
0xFN89 <eighteen089@gmail.com>
2016-03-01 22:21:37 +0530

    used properties file for smtp authentication

--- src/com/example/util/mailutil.java
+++ src/com/example/util/mailutil.java
@@ -3,20 +3,33 @@ package com.example.util;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.io.*;
 
 public class mailutil{
   public static void sendmail(String to,String from,String body)throws MessagingException{
     Properties props = new Properties();
+    InputStream input_stream=null;
     props.put("mail.smtp.host","smtp.gmail.com");
     props.put("mail.smtp.socketFactory.port","465");
     props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
     props.put("mail.smtp.auth","true");
     props.put("mail.smtp.port","465");
 
-
-    Session session = Session.getDefaultInstance(props,new javax.mail.Authenticator(){
+    try{
+      ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+      input_stream = classloader.getResourceAsStream("db.properties");
+      props.load(input_stream);
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    final String username = props.getProperty("smtp_user");
+    final String password = props.getProperty("smtp_password");
+    //Session session = Session.getInstance(props,null);
+    //Transport tr =session.getTransport("smtp");
+    //tr.connect("smtp.gmail.com",username,password);
+    Session session= Session.getInstance(props,new javax.mail.Authenticator(){
       protected PasswordAuthentication getPasswordAuthentication(){
-        return new PasswordAuthentication("sabgamehai@gmail.com","khushbu123");
+        return new PasswordAuthentication(username,password);
       }
     });
     session.setDebug(true);
@@ -31,5 +44,6 @@ public class mailutil{
     message.setRecipient(Message.RecipientType.TO,toaddress);
 
     Transport.send(message);
+    //tr.close();
   }
 }
